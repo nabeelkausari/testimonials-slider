aa1d33470739bc5baa21bdee4c4af228bcb1c956
nabeelkausari <nabeelkausari@gmail.com>
2020-05-16 13:26:40 +0530

    feat: add dynamic slides

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,74 @@
 import Head from 'next/head'
+import { useState } from "react";
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Frontend Mentor | Coding Bootcamp Testimonials Slider</title>
-      <link rel="icon" href="/favicon-32x32.png" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap" rel="stylesheet" />
-    </Head>
+const Home = () => {
+  const [slide, setSlide] = useState(0);
+  const slides = [
+    {
+      id: 1,
+      quote: `“ I’ve been interested in coding for a while but never taken the jump, until now.
+                I couldn’t recommend this course enough. I’m now in the job of my dreams and so
+                excited about the future. ”`,
+      name: "Tanya Sinclair",
+      designation: "UX Engineer",
+      image: "/image-tanya.jpg"
+    },
+    {
+      id: 2,
+      quote: `“ If you want to lay the best foundation possible I’d recommend taking this course.
+        The depth the instructors go into is incredible. I now feel so confident about
+        starting up as a professional developer. ”`,
+      name: "John Tarkpor",
+      designation: "Junior Front-end Developer",
+      image: "/image-john.jpg"
+    }
+  ];
+  const handlePrevSlide = () => {
+    if (slide === 0) return
+    setSlide(slide - 1);
+  }
 
-    <main>
-      <div className="slide-holder">
-        <div className="slide">
-          <div className="left">
-            <p className="content">
-              “ I’ve been interested in coding for a while but never taken the jump, until now.
-              I couldn’t recommend this course enough. I’m now in the job of my dreams and so
-              excited about the future. ”
-            </p>
-            <h5 className="author">Tanya Sinclair <span className="designation">UX Engineer</span></h5>
-          </div>
-          <div className="right">
-            <img className="person-img" alt="Tanya Image" src="/image-tanya.jpg"/>
-            <div className="nav">
-              <button className="prev"><img alt="Prev" src="/icon-prev.svg"/></button>
-              <button className="next"><img alt="Next" src="/icon-next.svg"/></button>
+  const handleNextSlide = () => {
+    if (slide === slides.length - 1) return
+    setSlide(slide + 1);
+  }
+  return (
+    <div className="container">
+      <Head>
+        <title>Frontend Mentor | Coding Bootcamp Testimonials Slider</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap" rel="stylesheet" />
+      </Head>
+
+      <main>
+        <div className="slide-holder">
+          <div className="slide">
+            <div className="left">
+              <p className="content">
+                {slides[slide].quote}
+              </p>
+              <h5 className="author">{slides[slide].name} <span className="designation">{slides[slide].designation}</span></h5>
+            </div>
+            <div className="right">
+              <img className="person-img" alt={slides[slide].name} src={slides[slide].image} />
             </div>
           </div>
+          <div className="nav">
+            <button onClick={handlePrevSlide} className="prev"><img alt="Prev" src="/icon-prev.svg"/></button>
+            <button onClick={handleNextSlide} className="next"><img alt="Next" src="/icon-next.svg"/></button>
+          </div>
         </div>
-      </div>
 
+      </main>
 
-      {/*“ If you want to lay the best foundation possible I’d recommend taking this course.*/}
-      {/*The depth the instructors go into is incredible. I now feel so confident about*/}
-      {/*starting up as a professional developer. ”*/}
+      {/*<footer>*/}
+      {/*  <div className="attribution">*/}
+      {/*    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.*/}
+      {/*    Coded by <a href="#">Nabeel Kausari</a>.*/}
+      {/*  </div>*/}
+      {/*</footer>*/}
 
-      {/*John Tarkpor*/}
-      {/*Junior Front-end Developer*/}
-    </main>
-
-    {/*<footer>*/}
-    {/*  <div className="attribution">*/}
-    {/*    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.*/}
-    {/*    Coded by <a href="#">Nabeel Kausari</a>.*/}
-    {/*  </div>*/}
-    {/*</footer>*/}
-
-    <style jsx>{`
+      <style jsx>{`
 
     .attribution { font-size: 11px; text-align: center; }
     .attribution a { color: hsl(228, 45%, 44%); }
@@ -54,24 +77,25 @@ const Home = () => (
       height: 100%;
       background-image: url("/pattern-curve.svg");
       background-repeat: no-repeat;
-      background-position: bottom left;
+      background-position: left bottom;
       display: flex;
       justify-content: center;
     }
     
     .slide-holder {
-      width: 1300px;
-      height: 650px;
+      position: relative;
+      width: 1295px;
+      height: 660px;
       background-image: url("/pattern-bg.svg");
       background-repeat: no-repeat;
-      background-position: top right;
+      background-position: right top;
       background-size: contain;
       margin-top: 50px;
     }
     
     .slide {
       position: relative;
-      width: 1120px;
+      width: 1110px;
       height: 75vh;
       margin: 0 auto;
       display: flex;
@@ -82,20 +106,30 @@ const Home = () => (
         width: 650px;
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        background-image: url("/pattern-quotes.svg");
-        background-repeat: no-repeat;
-        background-position: 18% 35%;
+        
         z-index: 1;
      }
      
      .content {
+        position: relative;
         font-size: 32px;
         font-weight: 300; 
-        line-height: 1.5;
-        margin-top: 200px;
-        margin-bottom: 0;
+        line-height: 1.4;
+        margin-top: 230px;
+        margin-bottom: 6px;
         color: hsl(240, 38%, 20%);
+       
+     }
+     .content:before {
+      content: "";
+      position: absolute;
+      width: 120px;
+      height: 100px;
+      left: 14.5%;
+      top: -30%;
+      background-image: url("/pattern-quotes.svg");
+      background-repeat: no-repeat;
+      background-size: contain;
      }
      
      .author {
@@ -103,8 +137,9 @@ const Home = () => (
       color: hsl(240, 38%, 20%);
      }
      .designation {
-      font-weight: 300;
+      font-weight: 500;
       color: hsl(240, 18%, 77%);
+      margin-left: 3px;
      }
      
      .right {
@@ -114,21 +149,28 @@ const Home = () => (
      }
      
      .person-img {
-      box-shadow: 1px 30px 40px rgba(0,0,0,.2);
+      margin-top: 2px;
+      box-shadow: 0 30px 30px rgba(0,0,0,.1);
      }
      
      .nav {
        position: absolute;
-       bottom: -5%;
-       left: 10%;
+       bottom: 30px;
+       left: 722px;
      }
      
      .nav button {
        background: #fff;
        border: none;
        outline: none;
-       padding: 18px;
-       width: 60px;
+       padding: 18px 10px;
+       width: 57px;
+       cursor: pointer;
+       transition: .5s all;
+     }
+     
+     .nav button:hover {
+        background: hsla(240, 18%, 77%, .5);
      }
      
      .prev {
@@ -141,7 +183,6 @@ const Home = () => (
         border-bottom-right-radius: 50%;
         border-top-right-radius: 50%;
         box-shadow: 5px 5px 10px rgba(0,0,0,.1);
-
      }
      
     
@@ -154,7 +195,7 @@ const Home = () => (
       }
     `}</style>
 
-    <style jsx global>{`
+      <style jsx global>{`
       html,
       body {
         padding: 0;
@@ -170,7 +211,8 @@ const Home = () => (
         box-sizing: border-box;
       }
     `}</style>
-  </div>
-)
+    </div>
+  )
+}
 
 export default Home
